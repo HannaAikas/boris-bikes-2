c97bebd2c1cb595b0d336067f3cb3c56e5483d98
Jodi <jodiallysonfoster@gmail.com>
2019-07-24 16:51:04 +0100

    Add functionality to return and check docked bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     bike = Bike.new
+  end
+  def dock_bike(bike)
+    @bike = bike
+  end
+  def bike_check
+    @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,18 @@ describe DockingStation do
     expect(bike.working?).to eq(true)
   end
 
+  it "docks a bike" do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike_check).to eq bike
+
+  end
+
 end
