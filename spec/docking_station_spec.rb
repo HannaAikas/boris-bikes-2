eb8f77887008b3beb7f9928ba3d0e6878d66387d
Hanna <hannaaikas@gmail.com>
2019-07-24 17:51:24 +0100

    Create test to raise error when no bikes available and user tries to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,24 +4,26 @@ require 'bike'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it "gets a bike, and expects the bike to be working" do
-    # below code tests creating a new instance of Bike when you call release_bike
-    bike = subject.release_bike
-    expect(bike.working?).to eq(true)
-  end
-
   it "docks a bike" do
     bike = Bike.new
     expect(subject.dock_bike(bike)).to eq bike
   end
 
-
-
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock_bike(bike)
     expect(subject.bike_check).to eq bike
+  end
 
+  describe "#release_bike" do
+    it "raises an error when release_bike if no bikes" do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
 end
